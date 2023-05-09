ae3234b8f5e644e2fdfec9158b51396e56e6cbd3
HydroGest <2445691453@qq.com>
2023-05-10 00:55:56 +0800

    Use !==

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,7 +73,7 @@ export default function AppBar({userObj}) {
               jc: "space-between",
             },
           }}>
-          { userObj!=false ? (
+          { userObj!==false ? (
             <userDrop/>
           )
           :(
